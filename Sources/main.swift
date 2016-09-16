ddd701ac637f3cf52c01d9bc12ea1313039bce07
Garrett Davidson <gdavids@purdue.edu>
2016-09-15 23:00:11 -0400

    Cleaned up main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -30,7 +30,7 @@ var routes = Routes()
 
 let testHost = "127.0.0.1"
 let testUser = "test"
-// PLEASE change to whatever your actual password is before running these tests
+
 let testPassword = "password"
 let testSchema = "HiLingualDB"
 let createMessagesTableQuery = "CREATE TABLE IF NOT EXISTS hl_chat_messages(" +
@@ -58,7 +58,6 @@ let createGoogleTableQuery = "CREATE TABLE IF NOT EXISTS hl_google_data(" +
                 "account_id VARCHAR(255), " +
                 "token TEXT);"
 
-
 let dataMysql = MySQL()
 
 routes.add(method: .get, uri: "/user/**", handler: handleUser)
@@ -66,67 +65,58 @@ routes.add(method: .get, uri: "/auth/**", handler: handleAuth)
 routes.add(method: .get, uri: "/asset/**", handler: handleAsset)
 routes.add(method: .get, uri: "/chat/**", handler: handleChat)
 
-
 // Add the routes to the server.
 server.addRoutes(routes)
 
 // Set a listen port of 8180
 server.serverPort = 8180
 
-// Set a document root.
-// This is optional. If you do not want to serve static content then do not set this.
-// Setting the document root will automatically add a static file handler for the route /**
-server.documentRoot = "./webroot"
-
 public func useMysql() {
-
-    // need to make sure something is available.
     guard dataMysql.connect(host: testHost, user: testUser, password: testPassword ) else {
         Log.info(message: "Failure connecting to data server \(testHost)")
         return
     }
 
     defer {
-        dataMysql.close()  // defer ensures we close our db connection at the end of this request
+        dataMysql.close()
     }
+
     if dataMysql.selectDatabase(named: testSchema) {
 
-	} else {
-		guard dataMysql.query(statement: "CREATE DATABASE \(testSchema) ") else {
-			print("Error creating db")
-			return
-		}
-    	guard dataMysql.query(statement: "USE \(testSchema) ") else {
-    		print("Error creating table1")
-			return
-    	}
-    	guard dataMysql.query(statement: "\(createUsersTableQuery)") else {
-    		print("Error creating table2")
-			return
-    	}
-    	guard dataMysql.query(statement: "\(createMessagesTableQuery)") else {
-    		print("Error creating table3")
-			return
-    	}
+    } else {
 
-		guard dataMysql.query(statement: "\(createFacebookTableQuery)") else {
-			print("Error creating table")
-			return
-		}
-
-		guard dataMysql.query(statement: "\(createGoogleTableQuery)") else {
-			print("Error creating table")
-			return
-		}
-
-
-
-
+	guard dataMysql.query(statement: "CREATE DATABASE \(testSchema) ") else {
+	    print("Error creating db")
+	    return
 	}
 
+    	guard dataMysql.query(statement: "USE \(testSchema) ") else {
+    	    print("Error creating table1")
+	    return
+    	}
 
+    	guard dataMysql.query(statement: "\(createUsersTableQuery)") else {
+    	    print("Error creating table2")
+	    return
+    	}
 
+    	guard dataMysql.query(statement: "\(createMessagesTableQuery)") else {
+    	    print("Error creating table3")
+	    return
+    	}
+
+	guard dataMysql.query(statement: "\(createFacebookTableQuery)") else {
+	    print("Error creating table")
+	    return
+	}
+
+	guard dataMysql.query(statement: "\(createGoogleTableQuery)") else {
+	    print("Error creating table")
+	    return
+	}
+    }
 }
+
 useMysql()
 
 // Gather command line options and further configure the server.
