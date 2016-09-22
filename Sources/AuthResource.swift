22f6aca8bffa01be2dd7ff2a6df2dfb25872dd80
Garrett Davidson <gdavids@purdue.edu>
2016-09-22 19:30:26 -0400

    Completed inspection on AuthResource

--- Sources/AuthResource.swift
+++ Sources/AuthResource.swift
@@ -3,69 +3,81 @@ import Foundation
 import PerfectHTTP
 
 func handleAuth(request: HTTPRequest, _ response: HTTPResponse) {
-	//parse uri and call relevant funtion
-	//response.setHeader(.contentType, value: "text/html")
-	print("start")
-	defer {
-		response.completed()
-	}
-	guard var urlString = request.urlVariables[routeTrailingWildcardKey] else {
-		return
-	}
-
-	//deserialize request JSON body to Dictionary
-	let jsonString: String! = request.postBodyString
-	do {
-		guard let result = try jsonString.jsonDecode() as? Dictionary<String, AnyObject> else {
-			print("invalid json string")
-			return
-		}
-
-		//validate request
-		guard let _ = request.header(HTTPRequestHeader.Name.authorization) else {
-			print("no auth parameter")
-			unauthorizedResponse(response: response)
-	        return
-	    }
-		//parse uri
-		urlString.remove(at: urlString.startIndex)
-		print(urlString)
-		if urlString == "login" {
-			loginWith(request: request, response, result)
-		} else if urlString == "register" {
-			registerWith(request: request, response, result)
-		} else if urlString == "logout" {
-			logoutWith(request: request, response, result)
-		}
-		response.completed()
-	} catch {
-		print("bad request")
-		badRequestResponse(response: response)
-		return
-	}
+    //parse uri and call relevant funtion
+    //response.setHeader(.contentType, value: "text/html")
+    print("start")
+    defer {
 	response.completed()
+    }
 
+    guard var urlString = request.urlVariables[routeTrailingWildcardKey] else {
+	return
+    }
+
+    //deserialize request JSON body to Dictionary
+    guard request.postBodyString != nil else {
+        return
+    }
+
+    guard let jsonString: String = request.postBodyString else {
+        print("Empty body")
+        return
+    }
+
+    do {
+	guard let result = try jsonString.jsonDecode() as? [String: AnyObject] else {
+	    print("invalid json string")
+	    return
+	}
+
+	//validate request
+	guard let _ = request.header(HTTPRequestHeader.Name.authorization) else {
+	    print("no auth parameter")
+	    unauthorizedResponse(response: response)
+	    return
+	}
+
+	//parse uri
+        if urlString.characters.first == "/" {
+	    urlString.remove(at: urlString.startIndex)
+        }
+
+	print(urlString)
+	if urlString == "login" {
+	    loginWith(request: request, response, result)
+	} else if urlString == "register" {
+	    registerWith(request: request, response, result)
+	} else if urlString == "logout" {
+	    logoutWith(request: request, response, result)
+	}
+    } catch {
+	print("bad request")
+	badRequestResponse(response: response)
+	return
+    }
 }
-func verifyAuthToken(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: Dictionary<String, AnyObject>) -> Bool {
-	//https://graph.facebook.com/me?access_token=xxxxxxxxxxxxxxxxx     FACEBOOK URL
-	//
+
+func verifyAuthToken(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: [String: AnyObject]) -> Bool {
+    //https://graph.facebook.com/me?access_token=xxxxxxxxxxxxxxxxx     FACEBOOK URL
+    //
     guard let token = requestBodyDic["authorityToken"] as? String else {
-   		print("no auth token sent")
-   		return false
+   	print("no auth token sent")
+   	return false
     }
     guard let auth = requestBodyDic["authority"] as? String else {
-   		print("no authority sent")
-   		return false
+   	print("no authority sent")
+   	return false
     }
     if auth == "FACEBOOK" {
     	return checkFacebookAuthority(token)
     } else if auth == "GOOGLE" {
     	return checkGoogleAuthority(token)
-	} else {
-		print("bad authority sent")
-   		return false
-	}
+    } else {
+	print("bad authority sent")
+   	return false
+    }
 }
+
 func checkGoogleAuthority(_ token: String) -> Bool {
     let scriptURL = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=\(token)"
     guard let myUrl = URL(string: scriptURL) else {
@@ -73,21 +85,18 @@ func checkGoogleAuthority(_ token: String) -> Bool {
     }
     let request1: URLRequest = URLRequest(url: myUrl)
     do {
-	     var response: URLResponse?
+	var response: URLResponse?
 
-	    try NSURLConnection.sendSynchronousRequest(request1, returning: &response)
+	try NSURLConnection.sendSynchronousRequest(request1, returning: &response)
     	if let httpResponse = response as? HTTPURLResponse {
-            if httpResponse.statusCode == 200 {
-        		return true
-        	} else {
-        		return false
-        	}
+            return httpResponse.statusCode == 200
         }
     } catch let error as NSError {
-         print(error.localizedDescription)
+        print(error.localizedDescription)
     }
     return false
 }
+
 func checkFacebookAuthority(_ token: String) -> Bool {
     let scriptURL = "https://graph.facebook.com/me?access_token=\(token)"
     guard let myUrl = URL(string: scriptURL) else {
@@ -95,104 +104,105 @@ func checkFacebookAuthority(_ token: String) -> Bool {
     }
     let request1: URLRequest = URLRequest(url: myUrl)
     do {
-	    var response: URLResponse?
+	var response: URLResponse?
 
         try NSURLConnection.sendSynchronousRequest(request1, returning: &response)
         if let httpResponse = response as? HTTPURLResponse {
-        	if httpResponse.statusCode == 200 {
-        		return true
-        	} else {
-        		return false
-        	}
+            return httpResponse.statusCode == 200
         }
     } catch let error as NSError {
-         print(error.localizedDescription)
+        print(error.localizedDescription)
     }
     return false
 }
-func loginWith(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: Dictionary<String, AnyObject>) {
-	print("Logging in user")
-	print(requestBodyDic["authority"])
-	print(requestBodyDic["authorityAccountId"])
-	print(requestBodyDic["authorityToken"])
-	if verifyAuthToken(request: request, response, requestBodyDic) {
-		guard let token = requestBodyDic["authorityToken"] as? String else {
-	   		print("no auth token sent")
-	   		badRequestResponse(response: response)
-	   		return
+func loginWith(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: [String: AnyObject]) {
+    print("Logging in user")
+    print(requestBodyDic["authority"])
+    print(requestBodyDic["authorityAccountId"])
+    print(requestBodyDic["authorityToken"])
+    if verifyAuthToken(request: request, response, requestBodyDic) {
+	guard let token = requestBodyDic["authorityToken"] as? String else {
+	    print("no auth token sent")
+	    badRequestResponse(response: response)
+	    return
     	}
     	guard let authID = requestBodyDic["authorityAccountId"] as? String else {
-	   		print("no authID sent")
-	   		badRequestResponse(response: response)
-	   		return
+	    print("no authID sent")
+	    badRequestResponse(response: response)
+	    return
     	}
     	loginUserWith(authAccountId: authID, sessionId: token)
-	} else {
-		errorResponse(response: response)
-	}
+    } else {
+	errorResponse(response: response)
+    }
 }
 
-func logoutWith(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: Dictionary<String, AnyObject>) {
-	print("Logging out user")
-	print(requestBodyDic["authority"])
-	print(requestBodyDic["authorityAccountId"])
-	print(requestBodyDic["authorityToken"])
-	if verifyAuthToken(request: request, response, requestBodyDic) {
-		guard let token = requestBodyDic["authorityToken"] as? String else {
-	   		print("no auth token sent")
-	   		badRequestResponse(response: response)
-	   		return
+func logoutWith(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: [String: AnyObject]) {
+    print("Logging out user")
+    print(requestBodyDic["authority"])
+    print(requestBodyDic["authorityAccountId"])
+    print(requestBodyDic["authorityToken"])
+    if verifyAuthToken(request: request, response, requestBodyDic) {
+	guard let token = requestBodyDic["authorityToken"] as? String else {
+	    print("no auth token sent")
+	    badRequestResponse(response: response)
+	    return
     	}
     	guard let authID = requestBodyDic["authorityAccountId"] as? String else {
-	   		print("no authID sent")
-	   		badRequestResponse(response: response)
-	   		return
+	    print("no authID sent")
+	    badRequestResponse(response: response)
+	    return
     	}
     	logoutUserWith(authAccountId: authID, sessionId: token)
-	} else {
-		errorResponse(response: response)
-	}
+    } else {
+	errorResponse(response: response)
+    }
 }
-func registerWith(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: Dictionary<String, AnyObject>) {
-	print("Registering new user")
-	print(requestBodyDic["authority"])
-	print(requestBodyDic["authorityAccountId"])
-	print(requestBodyDic["authorityToken"])
-	if verifyAuthToken(request: request, response, requestBodyDic) {
-		guard let token = requestBodyDic["authorityToken"] as? String else {
-	   		print("no auth token sent")
-	   		return
+
+func registerWith(request: HTTPRequest, _ response: HTTPResponse, _ requestBodyDic: [String: AnyObject]) {
+    print("Registering new user")
+    print(requestBodyDic["authority"])
+    print(requestBodyDic["authorityAccountId"])
+    print(requestBodyDic["authorityToken"])
+    if verifyAuthToken(request: request, response, requestBodyDic) {
+	guard let token = requestBodyDic["authorityToken"] as? String else {
+	    print("no auth token sent")
+	    return
     	}
-		guard let user = createUserWith(token: token) else {
-			print("database error")
-			return
-		}
-		let dict: [String: Any] = ["userId": user.getUserId(), "sessionId": user.getSessionToken()]
-		do {
-			try response.setBody(json: dict)
-		} catch {
-			print(error)
-		}
-	} else {
-		unauthorizedResponse(response: response)
+
+	guard let user = createUserWith(token: token) else {
+	    print("database error")
+	    return
 	}
 
-	//create new user in database
-	//respond to user
+	let dict: [String: Any] = ["userId": user.getUserId(), "sessionId": user.getSessionToken()]
+	do {
+	    try response.setBody(json: dict)
+	} catch {
+	    print(error)
+	}
+    } else {
+	unauthorizedResponse(response: response)
+    }
+
+    //create new user in database
+    //respond to user
 }
 
 func badRequestResponse(response: HTTPResponse) {
-	//400 code
-	response.setHeader(.contentType, value: "text/html")
-	response.status = HTTPResponseStatus.badRequest
+    //400 code
+    response.setHeader(.contentType, value: "text/html")
+    response.status = HTTPResponseStatus.badRequest
 }
+
 func unauthorizedResponse(response: HTTPResponse) {
-	//401 code
-	response.setHeader(.contentType, value: "text/html")
-	response.status = HTTPResponseStatus.unauthorized
+    //401 code
+    response.setHeader(.contentType, value: "text/html")
+    response.status = HTTPResponseStatus.unauthorized
 }
+
 func errorResponse(response: HTTPResponse) {
-	//500 code
-	response.setHeader(.contentType, value: "text/html")
-	response.status = HTTPResponseStatus.internalServerError
+    //500 code
+    response.setHeader(.contentType, value: "text/html")
+    response.status = HTTPResponseStatus.internalServerError
 }
