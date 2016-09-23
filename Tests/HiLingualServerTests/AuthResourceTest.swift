38de12fa3e385ea62f2cb2b963ebae27e00ef895
Garrett Davidson <gdavids@purdue.edu>
2016-09-22 22:20:01 -0400

    Add testing skeleton

---
+++ Tests/HiLingualServerTests/AuthResourceTest.swift
@@ -0,0 +1,37 @@
+import XCTest
+import PerfectNet
+import PerfectLib
+import PerfectHTTP
+
+class AuthResourceTests {
+
+    func testHandleAuth() {
+    }
+    func testVerifyAuthToken() {
+
+    }
+    func testCheckGoogleAuthority() {
+
+    }
+    func testCheckFacebookAuthority() {
+
+    }
+    func testLoginWithRequest() {
+
+    }
+    func testLogoutWithRequest() {
+
+    }
+    func testRegisterWithRequest() {
+
+    }
+    func testBadRequestResponse() {
+
+    }
+    func testUnauthorizedResponse() {
+
+    }
+    func testErrorResponse() {
+
+    }
+}
---
+++ Tests/HiLingualServerTests/TestHelpers.swift
@@ -0,0 +1,44 @@
+import XCTest
+import PerfectNet
+import PerfectLib
+import PerfectHTTP
+
+// Borrowed from PerfectHTTPRequests.swift
+class ShimHTTPRequest: HTTPRequest {
+    var method = HTTPMethod.get
+    var path = "/"
+    var queryParams = [(String, String)]()
+    var protocolVersion = (1, 1)
+    var remoteAddress = (host: "127.0.0.1", port: 8000 as UInt16)
+    var serverAddress = (host: "127.0.0.1", port: 8282 as UInt16)
+    var serverName = "my_server"
+    var documentRoot = "./webroot"
+    var connection = NetTCP()
+    var urlVariables = [String:String]()
+    var scratchPad = [String:Any]()
+    func header(_ named: HTTPRequestHeader.Name) -> String? { return nil }
+    func addHeader(_ named: HTTPRequestHeader.Name, value: String) {}
+    func setHeader(_ named: HTTPRequestHeader.Name, value: String) {}
+    var headers = AnyIterator<(HTTPRequestHeader.Name, String)> { return nil }
+    var postParams = [(String, String)]()
+    var postBodyBytes: [UInt8]? = nil
+    var postBodyString: String? = nil
+    var postFileUploads: [MimeReader.BodySpec]? = nil
+}
+
+class ShimHTTPResponse: HTTPResponse {
+    var request: HTTPRequest = ShimHTTPRequest()
+    var status: HTTPResponseStatus = .ok
+    var isStreaming = false
+    var bodyBytes = [UInt8]()
+    func header(_ named: HTTPResponseHeader.Name) -> String? { return nil }
+    func addHeader(_ named: HTTPResponseHeader.Name, value: String) {}
+    func setHeader(_ named: HTTPResponseHeader.Name, value: String) {}
+    var headers = AnyIterator<(HTTPResponseHeader.Name, String)> { return nil }
+    func addCookie(_: PerfectHTTP.HTTPCookie) {}
+    func appendBody(bytes: [UInt8]) {}
+    func appendBody(string: String) {}
+    func setBody(json: [String:Any]) throws {}
+    func push(callback: @escaping (Bool) -> ()) {}
+    func completed() {}
+}
